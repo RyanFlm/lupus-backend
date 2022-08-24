20d597aaf11afb68c0507f2f9ef7f9c3d82abeac
RyanFlm <50547464+RyanFlm@users.noreply.github.com>
2022-08-24 13:04:52 +0000

    Cleanup imap util

--- src/utils/imap.rs
+++ src/utils/imap.rs
@@ -1,10 +1,8 @@
-use std::net::TcpStream;
+use std::io::{Read, Write};
 
 use imap::{self, Session};
-use rustls_connector::{
-    rustls::{ClientConnection, StreamOwned},
-    RustlsConnector,
-};
+use rustls_connector::RustlsConnector;
+
 pub struct ConnectOptions {
     pub host: String,
     pub port: u16,
@@ -13,25 +11,31 @@ pub struct ConnectOptions {
     pub starttls: bool,
 }
 
-pub fn connect(
-    options: ConnectOptions,
-) -> Option<Session<StreamOwned<ClientConnection, TcpStream>>> {
+pub fn connect(options: ConnectOptions) -> Result<Session<impl Read + Write>, String> {
     let mut client_builder = imap::ClientBuilder::new(options.host, options.port);
 
+    // Enable STARTTLS if option is set
     if options.starttls {
         client_builder.starttls();
     }
 
+    // Connect client
     let client_result = client_builder.connect(|domain, tcp| {
-        let ssl_conn = RustlsConnector::new_with_native_certs()?;
-        Ok(ssl_conn.connect(domain, tcp)?)
+        let connector = RustlsConnector::new_with_native_certs().unwrap();
+        let connection = connector.connect(domain, tcp).unwrap();
+        Ok(connection)
     });
 
-    Some(
-        client_result
-            .expect("Failed to create client")
-            .login(options.username, options.password)
-            .map_err(|e| e.0)
-            .expect("Login failed"),
-    )
+    // Login to get active Session
+    let client = match client_result {
+        Ok(c) => c,
+        Err(_) => return Err(String::from("Failed to create client")),
+    };
+
+    let session_result = client.login(options.username, options.password).map_err(|e| e.0);
+
+    match session_result {
+        Ok(s) => Ok(s),
+        Err(_) => Err(String::from("Failed to login")),
+    }
 }
